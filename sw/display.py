865bb13f2e86536b2bf9f8971ffdfd460021a29c
wnew <wesley@ska.ac.za>
2012-11-28 17:40:29 +0200

    changes to the display app

--- sw/display.py
+++ sw/display.py
@@ -13,17 +13,9 @@ class MyTkApp(threading.Thread):
       self.root.quit()
    def run(self):
       self.top = Tkinter.Tk()
-      global C
-      C = Tkinter.Canvas(self.top, height=250, width=950)
-      global coord1
-      coord1 =  10, 10, 200, 200
-      global coord2
-      coord2 = 210, 10, 400, 200
-      global coord3
-      coord3 = 410, 10, 600, 200
-      global coord4
-      coord4 = 610, 10, 800, 200
-      C.pack()
+      global display
+      display = Tkinter.Canvas(self.top, height=250, width=950)
+      display.pack()
       self.top.mainloop()
       #self.root=Tkinter.Tk()
       #self.root.protocol("WM_DELETE_WINDOW", self.callback)
@@ -32,12 +24,54 @@ class MyTkApp(threading.Thread):
       #l.pack()
       #self.root.mainloop()
 
+class Timer:
+
+   def StartTimer(self):
+      self.TimerOffset = time.time()
+      self.LastTicked = 0
+      self.TimeWhenItWasPaused = 0
+      self.started = False
+      self.paused = False
+   
+   def Tick(self):
+      if self.paused is False:
+         NewTicked = time.time() - self.TimerOffset
+         diff = NewTicked - self.LastTicked
+         self.LastTicked = NewTicked
+         return diff
+      else:
+         print "Cannot Tick, Timer is paused"
+
+   def GetTime(self):
+      if self.started is False:
+         return 0
+      if self.paused is True:
+         return self.TimeWhenItWasPaused
+      else:
+         return time.time() - self.TimerOffset
+      
+   def Pause(self):
+      self.TimeWhenItWasPaused = time.time() - self.TimerOffset
+      self.paused = True
+
+   def Unpause(self):
+      self.TimerOffset = time.time() - self.TimeWhenItWasPaused
+      self.paused = False
+
 
 def reset_lights(self):
-  self.on_targ_red  = C.create_rectangle(coord1, fill="grey")
-  self.off_targ_red = C.create_rectangle(coord2, fill="grey")
-  self.off_targ_grn = C.create_rectangle(coord3, fill="grey")
-  self.on_targ_grn  = C.create_rectangle(coord4, fill="grey")
+   global coord1
+   coord1 =  10, 10, 200, 200
+   global coord2
+   coord2 = 210, 10, 400, 200
+   global coord3
+   coord3 = 410, 10, 600, 200
+   global coord4
+   coord4 = 610, 10, 800, 200
+   self.on_targ_red  = display.create_rectangle(coord1, fill="grey")
+   self.off_targ_red = display.create_rectangle(coord2, fill="grey")
+   self.off_targ_grn = display.create_rectangle(coord3, fill="grey")
+   self.on_targ_grn  = display.create_rectangle(coord4, fill="grey")
 
 
 #reset_lights()
@@ -47,23 +81,21 @@ class serial_coms():
       print self.ser.portstr       # check which port was really used
    def wait_for_hit(self):
       reset_lights(self)
-      for i in range(0,10000):
-         time.sleep(0.01);
-         s = ""
-         while s == '':
-            s = self.ser.readline()        # read (timeout)
-            if s != '':
-               #print s
-               if s == 'A':
-                  self.on_targ_red  = C.create_rectangle(coord1, fill="red")
-               if s == "C":
-                  self.off_targ_red = C.create_rectangle(coord2, fill="yellow")
-               if s == "D":
-                  self.off_targ_grn = C.create_rectangle(coord3, fill="yellow")
-               if s == "B":
-                  self.on_targ_grn  = C.create_rectangle(coord4, fill="green")
-               if s == "R":
-                  reset_lights(self)
+      s = ""
+      while 1:
+         s = self.ser.readline()        # read (timeout)
+         if s != '':
+            #print s
+            if s == 'A':
+               self.on_targ_red  = display.create_rectangle(coord1, fill="red")
+            if s == "C":
+               self.off_targ_red = display.create_rectangle(coord2, fill="yellow")
+            if s == "D":
+               self.off_targ_grn = display.create_rectangle(coord3, fill="yellow")
+            if s == "B":
+               self.on_targ_grn  = display.create_rectangle(coord4, fill="green")
+            if s == "R":
+               reset_lights(self)
       self.ser.close()
 
 
